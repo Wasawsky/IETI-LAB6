0ccd2b1eaea82a78eb00c27ca939c096f6f95b1c
Wasawsky <mjbc_20@hotmail.com>
2021-03-04 17:35:44 -0500

    First

---
+++ src/main/java/eci/ieti/TaskplannerAPI/TaskPlannerApiV1Application.java
@@ -0,0 +1,13 @@
+package eci.ieti.TaskplannerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerApiV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPlannerApiV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/eci/ieti/TaskplannerAPI/TaskPlannerApiV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.ieti.TaskplannerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlannerApiV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
